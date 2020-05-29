63f4aac50d9cb08b96bceb43228bcbccf6cb3f4d
Ecthon Borhis <ecthon@gmail.com>
2020-05-28 22:20:11 -0400

    Manipulando dados

--- instructors.js
+++ instructors.js
@@ -1,5 +1,27 @@
 const fs = require('fs')
 const data = require("./data.json")
+const { age } = require('./utils')
+
+// show
+exports.show = function(req, res) {
+        // req. params
+        const { id } = req.params
+
+        const foundInstructor = data.instructors.find(function(instructor) {
+            return instructor.id == id
+        })
+
+        if (!foundInstructor) return res.send("Instructor not found!")
+
+        const instructor = {
+            ...foundInstructor,
+            age: age(foundInstructor.birth),
+            services: foundInstructor.services.split(","),
+            created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+        }
+
+        return res.render("instructors/show", { instructor: foundInstructor })
+    }
     // create
 exports.post = function(req, res) {
     // req.query.
@@ -13,10 +35,23 @@ exports.post = function(req, res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, birth, name, services, gender } = req.body
 
-    data.structors.push(req.body)
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file error!")
