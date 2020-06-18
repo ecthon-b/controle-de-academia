cfbb6f2729ccf3d5302ccba628148e311f7112a6
Ecthon Borhis <ecthon@gmail.com>
2020-06-18 16:13:47 -0400

    Ajustando dados de cadastro de novo membro

--- controllers/members.js
+++ controllers/members.js
@@ -43,22 +43,20 @@ exports.post = function(req, res) {
         }
     }
 
-    let { avatar_url, birth, name, services, gender } = req.body
+    birth = Date.parse(req.body.birth)
 
-
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
+    let id = 1
+    const lastMember = data.members[data.members.length - 1]
+    
+    if (lastMember) {
+        id = lastMember.id + 1
+    }
 
 
     data.members.push({
         id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        services,
-        created_at
+        ...req.body,
+        birth
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
@@ -66,8 +64,6 @@ exports.post = function(req, res) {
 
         return res.redirect("/members")
     })
-
-    //return res.send(req.body)
 }
 
 // edit
